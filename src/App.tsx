5b6b93f3e5a400e5ed2069262a80f3c5ef68a492
jangwon <yoonjangwon94@gmail.com>
2024-07-26 14:36:10 +0900

    core: 초기 패키지 설정
    vite, tailwindcss, react, typesript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">fe1-vending</h1>
+    </div>
+  );
+}
+
+export default App;
